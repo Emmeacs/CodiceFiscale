1ca425d8c2f84f34439c905a08d90b87d2dd2f55
emmeacs <massimo.franzoso8895@gmail.com>
2018-04-26 15:02:57 +0200

    3 lettere cognomi

--- CodiceFiscale/Controllers/HomeController.cs
+++ CodiceFiscale/Controllers/HomeController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CodiceFiscale.Models;
 namespace CodiceFiscale.Controllers
 {
 	public class HomeController : Controller
 	{
+		DomainModel dm= new DomainModel();
 		public ActionResult Index()
 		{
+			
 			return View();
 		}
-
+		[HttpPost]
+		public ActionResult Genera(string InsertCognome)
+		{
+			string D = dm.Cognome(InsertCognome);
+			ViewBag.Cognome=D;
+			return View("Contact");
+		}
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
---
+++ CodiceFiscale/Models/Cognome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodiceFiscale.Models
+{
+	public class DomainModel
+	{
+		List<char> vocali = new List<char> {'A','E','I','O','U', 'a','e','i','o','u' };
+		public string Cognome(string cognome)
+		{
+			List<string> consonanti = new List<string>();
+			char[] parola = cognome.ToCharArray();
+			for(int i = 0; i < parola.Length; i++) {
+				if(!IsVowel(parola[i])) {
+					consonanti.Add(parola[i].ToString());
+					if(consonanti.Count == 3) {
+						return string.Join("",consonanti);
+					}
+				}
+			}
+			if(consonanti.Count < 3) {
+				foreach(char element in parola) {
+					if(IsVowel(element)) {
+						consonanti.Add(element.ToString());
+						if(consonanti.Count == 3) {
+							return string.Join("",consonanti);
+						}
+					}
+				}
+			}
+			if(consonanti.Count < 3) {
+				consonanti.Add(('x').ToString());
+				if(consonanti.Count == 3) {
+					return string.Join("",consonanti);
+				}
+			}
+			return string.Join("",consonanti);
+		}
+		public bool IsVowel(char c)
+		{
+			if(vocali.Contains(c)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+	}
+}
